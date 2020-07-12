40a5b1dd11caa2fd0bf8d2b51d3aa069fb1a3331
Ralph <19510622+RalXYZ@users.noreply.github.com>
2020-07-12 20:30:14 +0800

    Fixed "null Cat"

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/RalXYZ.jpg') {
+    if (mySrc === 'images/RalXYZ.jpg') {
         myImage.setAttribute('src', 'images/RalXYZ-red.jpg');
     } 
-    else if (mySrc === 'images/RalXYZ-red.jpg'){
+    else if (mySrc === 'images/RalXYZ-red.jpg') {
         myImage.setAttribute('src', 'images/RalXYZ.jpg');
     }
 }
@@ -16,7 +16,7 @@ let myHeading = document.querySelector('h1');
 function setUserName() {
     let myName = prompt('Please enter your name');
     localStorage.setItem('name', myName);
-    if(!myName || myName === null) {
+    if (!myName || myName === null) {
         myHeading.textContent = 'Cat';
     }
     else {
@@ -24,12 +24,12 @@ function setUserName() {
     }
 }
 
-if(!localStorage.getItem('name')) {
+if (!localStorage.getItem('name')) {
     setUserName();
 }
 else {
     let storedName = localStorage.getItem('name');
-    if(!storedName || storedName === null) {
+    if (!storedName || storedName === null  || storedName === 'null') {
         myHeading.textContent = 'Cat';
     }
     else {
